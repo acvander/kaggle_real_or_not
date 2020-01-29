ad424fa4077aa72fd4f48777e7b1cc5a54854c2b
Adam Vandergriff <vandy243@gmail.com>
2020-01-29 13:19:29 -0500

    model separated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from train_model import train_model
 from preprocessing import preprocess
 from metrics.CustomF1 import CustomF1Score
 from typing import Dict, Sequence, Tuple
@@ -22,45 +23,15 @@ MODEL_PATH = './tmp/model.h5'
 def main():
     train_df = pd.read_csv(TRAIN_PATH)
     test_df = pd.read_csv(TEST_PATH)
-    columns = train_df.columns.to_list()
-    example = train_df.iloc[100].to_list()
 
     train_data, test_data, embeddings, pad_lens = preprocess(
         train_df, test_df, num_words=NUM_WORDS)
 
-    model = build_model(
-        embeddings,
-        pad_lens,
-        tokenizer_len=NUM_WORDS,
-    )
-
-    metrics = []
-    # f1score = CustomF1Score()
-    # metrics.append(f1score)
-    metrics.append(tf.keras.metrics.Recall())
-    # metrics.append(FBetaScore(1))
-    # metrics.append(F1Score(2))
-    model.compile(loss="binary_crossentropy",
-                  optimizer='adam',
-                  metrics=metrics)
-
-    callbacks = []
-    checkpoint = tf.keras.callbacks.ModelCheckpoint(MODEL_PATH,
-                                                    'val_recall',
-                                                    verbose=1,
-                                                    save_best_only=True,
-                                                    mode='max')
-    callbacks.append(checkpoint)
-
-    model.fit(train_data,
-              train_data['target'],
-              validation_split=0.2,
-              epochs=25,
-              batch_size=128,
-              verbose=1,
-              callbacks=callbacks)
-
-    model.save('./tmp/model.h5')
+    model = train_model(train_data,
+                        embeddings,
+                        pad_lens,
+                        num_words=NUM_WORDS,
+                        model_path=MODEL_PATH)
     return
 
 
---
+++ train_model.py
@@ -0,0 +1,48 @@
+from typing import Dict
+
+import numpy as np
+import tensorflow as tf
+
+from build_model import build_model
+
+
+def train_model(train_data: Dict,
+                embeddings: np.array,
+                pad_lens: Dict,
+                num_words: int = 1000,
+                model_path: str = './tmp/model.h5') -> tf.keras.models.Model:
+    model = build_model(
+        embeddings,
+        pad_lens,
+        tokenizer_len=num_words,
+    )
+
+    metrics = []
+    # f1score = CustomF1Score()
+    # metrics.append(f1score)
+    metrics.append(tf.keras.metrics.Recall())
+    # metrics.append(FBetaScore(1))
+    # metrics.append(F1Score(2))
+    model.compile(loss="binary_crossentropy",
+                  optimizer='adam',
+                  metrics=metrics)
+
+    callbacks = []
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(model_path,
+                                                    'val_recall',
+                                                    verbose=1,
+                                                    save_best_only=True,
+                                                    mode='max')
+    callbacks.append(checkpoint)
+
+    model.fit(train_data,
+              train_data['target'],
+              validation_split=0.2,
+              epochs=25,
+              batch_size=128,
+              verbose=1,
+              callbacks=callbacks)
+
+    model.save('./tmp/model.h5')
+
+    return model
