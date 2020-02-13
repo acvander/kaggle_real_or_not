fe730689f03a3e28476eb0aa96afedf8246e7284
Adam Vandergriff <vandy243@gmail.com>
2020-02-13 18:47:46 -0500

    discovered work around for memory issues

--- training/train_bert.py
+++ training/train_bert.py
@@ -5,7 +5,7 @@ import time
 
 from absl import logging
 import tensorflow as tf
-from tensorflow.keras.models import clone_model
+from tensorflow.keras.models import clone_model, load_model
 from tensorflow.keras.utils import plot_model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
@@ -34,7 +34,8 @@ def compile_model(model,
 
 def create_callbacks(learn_rate: float = 0.001,
                      model_path: str = './tmp/model.h5',
-                     epochs: int = 25):
+                     epochs: int = 25,
+                     min_delta: float = 0.01):
     callbacks = []
     checkpoint = ModelCheckpoint(model_path,
                                  'val_f1_score',
@@ -43,23 +44,25 @@ def create_callbacks(learn_rate: float = 0.001,
                                  mode='max')
     callbacks.append(checkpoint)
 
-    # early_stop_patience = epochs // 3
-    # early_stop = EarlyStopping('val_f1_score',
-    #                            min_delta=0.01,
-    #                            mode='max',
-    #                            patience=early_stop_patience,
-    #                            verbose=1)
-    # callbacks.append(early_stop)
+    early_stop_patience = epochs // 3
+    logging.info('early stop patience {}'.format(early_stop_patience))
+    early_stop = EarlyStopping('val_f1_score',
+                               min_delta=min_delta,
+                               mode='max',
+                               patience=early_stop_patience,
+                               verbose=1)
+    callbacks.append(early_stop)
 
-    # reduce_lr_patience = early_stop_patience // 2 - 1
-    # reduce_lr = ReduceLROnPlateau('val_f1_score',
-    #                               factor=0.1,
-    #                               min_delta=0.01,
-    #                               mode='max',
-    #                               patience=reduce_lr_patience,
-    #                               min_lr=learn_rate * 10**-3,
-    #                               verbose=1)
-    # callbacks.append(reduce_lr)
+    reduce_lr_patience = early_stop_patience // 2 - 1
+    logging.info('reduce lr patience {}'.format(reduce_lr_patience))
+    reduce_lr = ReduceLROnPlateau('val_f1_score',
+                                  factor=0.1,
+                                  min_delta=min_delta,
+                                  mode='max',
+                                  patience=reduce_lr_patience,
+                                  min_lr=learn_rate * 10**-3,
+                                  verbose=1)
+    callbacks.append(reduce_lr)
     return callbacks
 
 
@@ -80,6 +83,11 @@ def train_bert(model_dir: str = './tmp/bert_default',
 
     logging.info('loading model')
 
+    bert_layer = hub.KerasLayer(bert_url, trainable=True)
+    model = build_bert_model(bert_layer, max_len=max_token_len)
+    initial_model_path = os.path.join(model_dir, 'initial_weights.h5')
+    model.save_weights(initial_model_path)
+
     os.makedirs(model_dir, exist_ok=True)
 
     splitter = StratifiedKFold(n_splits=3, shuffle=shuffle, random_state=2020)
@@ -95,8 +103,9 @@ def train_bert(model_dir: str = './tmp/bert_default',
     for i, (valid_idxs, train_idxs) in enumerate(k_folds):
         logging.info('training fold #{}'.format(i + 1))
 
-        bert_layer = hub.KerasLayer(bert_url, trainable=True)
-        model = build_bert_model(bert_layer, max_len=max_token_len)()
+        # load initial weights and reset states
+        model.load_weights(initial_model_path)
+        model.reset_states()
 
         model_path = os.path.join(model_dir, '{}_{}.h5'.format(model_name, i))
 
@@ -115,7 +124,7 @@ def train_bert(model_dir: str = './tmp/bert_default',
         }
         fold_valid_output = train_output[valid_idxs]
 
-        callbacks = create_callbacks(model_path=model_path)
+        callbacks = create_callbacks(model_path=model_path, epochs=epochs)
 
         history = model.fit(fold_train_input,
                             fold_train_output,
@@ -128,13 +137,6 @@ def train_bert(model_dir: str = './tmp/bert_default',
             history.history,
             '{}_{}.png'.format(os.path.join(model_dir, fig_name), i))
         histories.append(history)
-
-        del bert_layer
-        del model
-        tf.keras.backend.clear_session()
-        gc.collect()
-        logging.info('waiting for memory clear')
-        time.sleep(60)
 
     plot_k_fold_data(
         histories, '{}_combined.png'.format(os.path.join(model_dir, fig_name)))
